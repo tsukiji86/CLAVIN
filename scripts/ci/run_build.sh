45f25431fc5439e2bba55c1fabf30c7829eda3b5
Richard Clayton <rclayton@bericotechnologies.com>
2013-06-27 08:01:39 -0400

    Fixed the Travis CI build script to point to the right indexer

--- scripts/ci/run_build.sh
+++ scripts/ci/run_build.sh
@@ -2,5 +2,6 @@
 curl http://download.geonames.org/export/dump/allCountries.zip -o allCountries.zip
 unzip allCountries.zip
 mvn compile
-mvn exec:java -Dexec.mainClass="com.berico.clavin.index.IndexDirectoryBuilder" -Dexec.args="-Xmx2g"
+export MAVEN_OPTS="-Xmx2g"
+mvn exec:java -Dexec.mainClass="com.berico.clavin.resolver.impl.lucene.GeonamesIndexBuilder" -Dexec.args="./IndexDirectory allCountries.txt"
 mvn package
