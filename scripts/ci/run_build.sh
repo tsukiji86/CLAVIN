a5867be1e224f3d4745ce2413577c653797e9311
Richard Clayton <rclayton@bericotechnologies.com>
2013-06-27 09:08:46 -0400

    Hoping to fix Travis CI permissions issue

--- scripts/ci/run_build.sh
+++ scripts/ci/run_build.sh
@@ -2,6 +2,5 @@
 curl http://download.geonames.org/export/dump/allCountries.zip -o allCountries.zip
 unzip allCountries.zip
 mvn compile
-export MAVEN_OPTS="-Xmx2g"
 mvn exec:java -Dexec.mainClass="com.berico.clavin.resolver.impl.lucene.GeonamesIndexBuilder" -Dexec.args="./IndexDirectory allCountries.txt"
 mvn package
