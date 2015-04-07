8dcb385272636c08319ff2b9971b83e58b900040
ppiersonbt <ppierson@bericotechnologies.com>
2015-04-07 09:05:26 -0400

    Update run_build.sh
    added MAVEN_OPTS="-Xmx4g" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.index.IndexDirectoryBuilder" back in

--- script/ci/run_build.sh
+++ script/ci/run_build.sh
@@ -2,5 +2,6 @@
 curl http://download.geonames.org/export/dump/allCountries.zip -o allCountries.zip
 unzip allCountries.zip
 mvn compile
+MAVEN_OPTS="-Xmx4g" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.index.IndexDirectoryBuilder"
 mvn package
 
