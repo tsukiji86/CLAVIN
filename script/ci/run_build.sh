9dcc5b00783a6ee5f47d2c78a5ea11023ae3c8a4
Patrick Pierson <ppierson@bericotechnologies.com>
2015-04-07 11:37:57 -0400

    testing updates to travic-ci build, not including index building

--- script/ci/run_build.sh
+++ script/ci/run_build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
-curl http://download.geonames.org/export/dump/allCountries.zip -o allCountries.zip
-unzip allCountries.zip
 mvn compile
-MAVEN_OPTS="-Xmx4g" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.index.IndexDirectoryBuilder"
-mvn package
+mvn package -DskipTests=true -Dgpg.skip=true
 
