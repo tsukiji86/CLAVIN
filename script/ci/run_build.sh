cde243e536834aa0cca94f4e1c2eb1ecb0f03f58
Patrick Pierson <ppierson@bericotechnologies.com>
2015-04-08 07:31:35 -0400

    testing smaller index

--- script/ci/run_build.sh
+++ script/ci/run_build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 curl http://download.geonames.org/export/dump/allCountries.zip -o allCountries.zip
 unzip allCountries.zip
+head -n 100 allCountries.txt > small_index.txt
+tail -n 100 allCountries.txt >> small_index.txt
+mv -f small_index.txt allCountries.txt
 mvn compile
 MAVEN_OPTS="-Xmx4g" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.index.IndexDirectoryBuilder"
 mvn package
