3f0b8d6c352ef128b42cf8737495c40caead214f
Patrick Pierson <ppierson@bericotechnologies.com>
2015-04-08 08:11:30 -0400

    added more places to smaller index for tests

--- script/ci/run_build.sh
+++ script/ci/run_build.sh
@@ -2,9 +2,20 @@
 curl http://download.geonames.org/export/dump/allCountries.zip -o allCountries.zip
 unzip allCountries.zip
 head -n 100 allCountries.txt > small_index.txt
-grep Reston allCountries.txt >> small_index.txt
+grep Virginia allCountries.txt >> small_index.txt
+grep Vermont allCountries.txt >> small_index.txt
+grep Illinois allCountries.txt >> small_index.txt
+grep Missouri allCountries.txt >> small_index.txt
+grep Oregon allCountries.txt >> small_index.txt
+grep Springfield allCountries.txt >> small_index.txt
+grep Massachusetts allCountries.txt >> small_index.txt
 grep Bostonn allCountries.txt >> small_index.txt
+grep lond allCountries.txt >> small_index.txt
+grep "Straßenhaus" allCountries.txt >> small_index.txt
 grep "Hong Kong Special Administrative Region" allCountries.txt >> small_index.txt
+grep "Clipperton Island" allCountries.txt >> small_index.txt
+grep "United States" allCountries.txt >> small_index.txt
+grep "Soviet Union" allCountries.txt >> small_index.txt
 tail -n 100 allCountries.txt >> small_index.txt
 mv -f small_index.txt allCountries.txt
 mvn compile
