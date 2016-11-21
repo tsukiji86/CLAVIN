f73c741f33a01b91aa5b06e71860bc354ef3010d
David Penner <dpenner@bericotechnologies.com>
2016-11-21 14:32:49 -0500

    Added OS detection for Windows newline support

--- src/test/java/com/bericotech/clavin/util/TextUtilsTest.java
+++ src/test/java/com/bericotech/clavin/util/TextUtilsTest.java
@@ -49,9 +49,16 @@ public class TextUtilsTest {
      */
     @Test
     public void testFileToString() throws IOException {
-        // TODO: fix this test so it works with Windows newline chars
+
         File inputFile = new File("src/test/resources/sample-docs/SampleText.txt");
-        String expectedString = "Line1 word2\nLine2 word2 word3\nLine3";
+        String osName = System.getProperty("os.name", "generic").toLowerCase();
+        String expectedString;
+        if(osName.contains("windows")) {
+            expectedString = "Line1 word2\r\nLine2 word2 word3\r\nLine3";
+        }
+        else {
+            expectedString = "Line1 word2\nLine2 word2 word3\nLine3";
+        }
         String fileString = TextUtils.fileToString(inputFile);
         assertEquals("wrong output from fileToString", expectedString, fileString);
     }
